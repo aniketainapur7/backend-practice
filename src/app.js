a6ff8fbbcaeb29aed35f4656d9639ad64f5e883c
AniketAinapur <aniket.ainapur23@vit.edu>
2025-02-21 03:20:15 +0530

    Async Handler

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import express from 'express'
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}));
+
+app.use(cookieParser);
+app.use(express.urlencoded({limit: "16kb"}))
+app.use(express.json({limit: "16kb"}))
+app.use(express.static('public'));
+
+
+
+export {app}
